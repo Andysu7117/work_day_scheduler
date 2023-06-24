e5e37707e8c2aaeb6dae770965753859dc86619e
Andy <andy.su7117@gmail.com>
2023-06-24 18:27:17 +1000

    added timeblocks and save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,30 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var saveBtn = $('.saveBtn');
+  saveBtn.on('click', function() {
+    var specificTimeBlock = $(this).parent().attr('id');
+    var timeBlockText = $(this).siblings('.description').val();
+    eventsSaved[specificTimeBlock] = timeBlockText;
+    localStorage.setItem("events", JSON.stringify(eventsSaved))
+  })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  var times = [
+    { id: 'hour-9', hour: 9},
+    { id: 'hour-10', hour: 10},
+    { id: 'hour-11', hour: 11},
+    { id: 'hour-12', hour: 12},
+    { id: 'hour-13', hour: 13},
+    { id: 'hour-14', hour: 14},
+    { id: 'hour-15', hour: 15},
+    { id: 'hour-16', hour: 16},
+    { id: 'hour-17', hour: 17},
+  ];
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
