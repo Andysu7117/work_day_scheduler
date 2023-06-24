b140f592899742fcfc38964a4591830d01a1fd86
Andy <andy.su7117@gmail.com>
2023-06-24 19:21:34 +1000

    code for adding past, present, future classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var times = [
+    { id: 'hour-9', hour: 9},
+    { id: 'hour-10', hour: 10},
+    { id: 'hour-11', hour: 11},
+    { id: 'hour-12', hour: 12},
+    { id: 'hour-13', hour: 13},
+    { id: 'hour-14', hour: 14},
+    { id: 'hour-15', hour: 15},
+    { id: 'hour-16', hour: 16},
+    { id: 'hour-17', hour: 17},
+  ];
+
+  var eventsSaved = JSON.parse(localStorage.getItem("events")) || {};
+  
+  // Current date
+  var currentDay = $('#currentDay');
+  var currentDate = dayjs().format('dddd, MMMM, d');
+  currentDay.text(currentDate);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -22,17 +40,21 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  var times = [
-    { id: 'hour-9', hour: 9},
-    { id: 'hour-10', hour: 10},
-    { id: 'hour-11', hour: 11},
-    { id: 'hour-12', hour: 12},
-    { id: 'hour-13', hour: 13},
-    { id: 'hour-14', hour: 14},
-    { id: 'hour-15', hour: 15},
-    { id: 'hour-16', hour: 16},
-    { id: 'hour-17', hour: 17},
-  ];
+  // loop through all time blocks
+  var currentHour = dayjs().format('H')
+  times.forEach(function(time) {
+    var timeBlockHour = $('#' + time.id)
+
+    if (time.hour < currentHour) {
+      timeBlockHour.addClass('past')
+    } else if ( time.hour === currentHour) {
+      timeBlockHour.addClass('present')
+    } else {
+      timeBlockHour.addClass('future')
+    }
+  })
+  
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
